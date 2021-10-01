e8050c530be9d71762d0506ac9f870fc01592637
Guilherme Ferreira da Silva <67923079+Guilherme-Ferr@users.noreply.github.com>
2021-10-01 07:49:23 -0300

    update base url

--- app/src/main/java/com/example/smart/api/UrlApi.kt
+++ app/src/main/java/com/example/smart/api/UrlApi.kt
@@ -2,6 +2,6 @@ package com.example.flowtrandingsystem.gui.api
 
 class UrlApi {
     companion object{
-        const val BASE_URL = "http://192.168.50.22:3333"
+        const val BASE_URL = ""
     }
 }
